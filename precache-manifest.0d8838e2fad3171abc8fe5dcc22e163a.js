9195d04656ca4d66aaf259e591dce4592297fbd3
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-09-26 12:16:41 -0400

    Updates

---
+++ precache-manifest.0d8838e2fad3171abc8fe5dcc22e163a.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c781cec6605978a8196935d8b4e52b1",
+    "url": "/lios/index.html"
+  },
+  {
+    "revision": "7cfc27ec7134bc712054",
+    "url": "/lios/static/css/4.8921c4c4.chunk.css"
+  },
+  {
+    "revision": "0bfd2d9b98671b0f3405",
+    "url": "/lios/static/css/main.9a0cfab0.chunk.css"
+  },
+  {
+    "revision": "bb49b1bcefaa2f86d919",
+    "url": "/lios/static/js/0.55b8e71b.chunk.js"
+  },
+  {
+    "revision": "450d686a98f1977c7b46",
+    "url": "/lios/static/js/10.29600b77.chunk.js"
+  },
+  {
+    "revision": "d7884e862ba55db54fee",
+    "url": "/lios/static/js/11.045a18f2.chunk.js"
+  },
+  {
+    "revision": "0e7b912e500da0f142fd",
+    "url": "/lios/static/js/12.705a3dcd.chunk.js"
+  },
+  {
+    "revision": "0c5e7b30d15b9d41dbbd",
+    "url": "/lios/static/js/13.cd315fee.chunk.js"
+  },
+  {
+    "revision": "b5ef3dc56f3302944995",
+    "url": "/lios/static/js/14.36310d2f.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/lios/static/js/14.36310d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa4c05db88fa7979619",
+    "url": "/lios/static/js/15.4d4f077e.chunk.js"
+  },
+  {
+    "revision": "82979274aee6330a9916",
+    "url": "/lios/static/js/16.6bfeef45.chunk.js"
+  },
+  {
+    "revision": "a27c8970675b2c03d684",
+    "url": "/lios/static/js/17.c328bfa1.chunk.js"
+  },
+  {
+    "revision": "13a139f8b2380b21a8b4",
+    "url": "/lios/static/js/18.621ef1b4.chunk.js"
+  },
+  {
+    "revision": "edc5e73ceaaf12f56d70",
+    "url": "/lios/static/js/19.b1c9f1ae.chunk.js"
+  },
+  {
+    "revision": "5a8ea311110cdb05d73c",
+    "url": "/lios/static/js/20.076b97e3.chunk.js"
+  },
+  {
+    "revision": "db7555ec71eb404211d5",
+    "url": "/lios/static/js/21.6e04dcc1.chunk.js"
+  },
+  {
+    "revision": "8f8cca830ab5a5d65bd8",
+    "url": "/lios/static/js/22.f5a902a8.chunk.js"
+  },
+  {
+    "revision": "1eb05301f0da6ccd46cb",
+    "url": "/lios/static/js/23.e08290b4.chunk.js"
+  },
+  {
+    "revision": "c73eb45f8b09163c71f9",
+    "url": "/lios/static/js/24.584f0778.chunk.js"
+  },
+  {
+    "revision": "e641f5a9611519ad27c2",
+    "url": "/lios/static/js/25.de27bb96.chunk.js"
+  },
+  {
+    "revision": "42802061eba82d402283",
+    "url": "/lios/static/js/26.2c898f0f.chunk.js"
+  },
+  {
+    "revision": "3397e638de9b3ce338c3",
+    "url": "/lios/static/js/27.891cc64f.chunk.js"
+  },
+  {
+    "revision": "f7a66a147dbcfa88915c",
+    "url": "/lios/static/js/28.bc8deda6.chunk.js"
+  },
+  {
+    "revision": "5eda1421e7e8fb753beb",
+    "url": "/lios/static/js/29.a71cf81f.chunk.js"
+  },
+  {
+    "revision": "06bd4fd70e8668ef960c",
+    "url": "/lios/static/js/30.97a4f9fb.chunk.js"
+  },
+  {
+    "revision": "c64b0938a1e362b5abf8",
+    "url": "/lios/static/js/31.9112ae31.chunk.js"
+  },
+  {
+    "revision": "1ca3aa37ccca013f0e08",
+    "url": "/lios/static/js/32.fe8029ba.chunk.js"
+  },
+  {
+    "revision": "c6fdebb3d18e73de8577",
+    "url": "/lios/static/js/33.9ca2a0f5.chunk.js"
+  },
+  {
+    "revision": "1196b6b5ee727ddec562",
+    "url": "/lios/static/js/34.378c7136.chunk.js"
+  },
+  {
+    "revision": "09944c27a97b512b51db",
+    "url": "/lios/static/js/35.59d2ca20.chunk.js"
+  },
+  {
+    "revision": "c7c9af5e2393a7856c58",
+    "url": "/lios/static/js/36.e00d404e.chunk.js"
+  },
+  {
+    "revision": "a65c1334b4ee0494cd37",
+    "url": "/lios/static/js/37.c1a9cdcc.chunk.js"
+  },
+  {
+    "revision": "812ba06bf79957eb6eec",
+    "url": "/lios/static/js/38.1c941cdd.chunk.js"
+  },
+  {
+    "revision": "e6405cac75404c142a79",
+    "url": "/lios/static/js/39.c8c88d60.chunk.js"
+  },
+  {
+    "revision": "7cfc27ec7134bc712054",
+    "url": "/lios/static/js/4.fc064e40.chunk.js"
+  },
+  {
+    "revision": "29a3abf0ca107cb60c35f965750445c1",
+    "url": "/lios/static/js/4.fc064e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41b74d34829f01ffc2a",
+    "url": "/lios/static/js/40.cd2278da.chunk.js"
+  },
+  {
+    "revision": "ec997213ee5ae434f916",
+    "url": "/lios/static/js/41.f5cb540a.chunk.js"
+  },
+  {
+    "revision": "e938af7fdf42aeb57645",
+    "url": "/lios/static/js/42.bb6c7fd3.chunk.js"
+  },
+  {
+    "revision": "611c822cd1048060c1e8",
+    "url": "/lios/static/js/43.464da0b7.chunk.js"
+  },
+  {
+    "revision": "48c8980db9f11029d870",
+    "url": "/lios/static/js/44.4fcb4144.chunk.js"
+  },
+  {
+    "revision": "63446171b738e299041f",
+    "url": "/lios/static/js/45.ed7ea1d3.chunk.js"
+  },
+  {
+    "revision": "527831b2f8a99d7444fc",
+    "url": "/lios/static/js/46.505e03cc.chunk.js"
+  },
+  {
+    "revision": "18738ea4aa07d677cc9d",
+    "url": "/lios/static/js/47.5037c30b.chunk.js"
+  },
+  {
+    "revision": "0f38a4fc9a41bd460ac8",
+    "url": "/lios/static/js/48.d09d9530.chunk.js"
+  },
+  {
+    "revision": "2bfb8cf3a6b2161e222b",
+    "url": "/lios/static/js/49.2c6b5dc6.chunk.js"
+  },
+  {
+    "revision": "2d90f24384dec88c8186",
+    "url": "/lios/static/js/5.ae55c121.chunk.js"
+  },
+  {
+    "revision": "83ab9be52da3e0e619d0",
+    "url": "/lios/static/js/50.44d2aa7d.chunk.js"
+  },
+  {
+    "revision": "642736b1eb4dedee4ce0",
+    "url": "/lios/static/js/51.85d19611.chunk.js"
+  },
+  {
+    "revision": "376d004ffde10181ed48",
+    "url": "/lios/static/js/52.9ce1cdbd.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/lios/static/js/52.9ce1cdbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bedb3c028055c04abf5",
+    "url": "/lios/static/js/53.bffeac58.chunk.js"
+  },
+  {
+    "revision": "c0fd008545df0eda2ddb",
+    "url": "/lios/static/js/54.525550ae.chunk.js"
+  },
+  {
+    "revision": "969b9f355f23b2bfb79d",
+    "url": "/lios/static/js/55.fb464d05.chunk.js"
+  },
+  {
+    "revision": "32bf5e6fd9048b3e4337",
+    "url": "/lios/static/js/56.d0d5dba7.chunk.js"
+  },
+  {
+    "revision": "e755f7e9bfaaa23ba8ac",
+    "url": "/lios/static/js/57.029bb9d9.chunk.js"
+  },
+  {
+    "revision": "7830998777c0d910920a",
+    "url": "/lios/static/js/58.96d2f0f4.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/lios/static/js/58.96d2f0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a7281ce2029359c7bb",
+    "url": "/lios/static/js/6.c2490f20.chunk.js"
+  },
+  {
+    "revision": "745e215c86ba795d2db7",
+    "url": "/lios/static/js/7.919e206c.chunk.js"
+  },
+  {
+    "revision": "a6e642187ccfdf346a93",
+    "url": "/lios/static/js/8.a2c08c25.chunk.js"
+  },
+  {
+    "revision": "8bf7920a0f2c0e0228f6",
+    "url": "/lios/static/js/9.a5d31602.chunk.js"
+  },
+  {
+    "revision": "0bfd2d9b98671b0f3405",
+    "url": "/lios/static/js/main.5d2b06f9.chunk.js"
+  },
+  {
+    "revision": "3afcf0710b8a90b99061",
+    "url": "/lios/static/js/polyfills-css-shim.207dde7e.chunk.js"
+  },
+  {
+    "revision": "0b957822bfddcac69217",
+    "url": "/lios/static/js/runtime-main.057e6447.js"
+  }
+]);
---
+++ static/js/53.bffeac58.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonplios=this.webpackJsonplios||[]).push([[53],{701:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(13),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.j)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(e.g)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.bffeac58.chunk.js.map

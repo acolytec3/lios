1f2990e18a482f4be2828a7c8e52e78916b41c99
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-09-26 12:11:40 -0400

    Add additional  star server addresses

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,9 @@ const options: any = {
     streamMuxer: [mplex],
   },
   addresses: {
-    listen: ['/ip4/192.168.1.194/tcp/13579/wss/p2p-webrtc-star'],
+    listen: ['/ip4/192.168.1.194/tcp/13579/wss/p2p-webrtc-star',
+    '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+    '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'],
   }
 }
 
